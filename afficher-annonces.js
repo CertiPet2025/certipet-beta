de50b0db867333fc3e7588f04fb3a406173d7161
CertiPet2025 <delphine.dupenloup74160@gmail.com>
2025-07-21 18:36:37 +0100

    Déplacement de annonces.html à la racine

--- afficher-annonces.js
+++ afficher-annonces.js
@@ -31,7 +31,6 @@ function creerCarte(annonce) {
   image.src = annonce.fichierPhoto;
   image.alt = "Photo de l'animal";
 
-  const fichierPhoto = reader.result; // quand le FileReader lit l’image
 
   const certificat = document.createElement("a");
   certificat.href = annonce.fichierCertificat;
